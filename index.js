5ce5486154cab6aa0d431929eb9bcc9c551c907b
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-08-21 02:56:07 +0530

    Added Minting cards with crossmint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const crypto = require("crypto");
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config;
+const axios = require("axios");
 
 const app = express();
 
@@ -42,6 +43,228 @@ app.get("/", (req, res) => {
   const temp = dataFetch;
   dataFetch = "Nothing";
   res.send(temp);
+});
+
+app.post("/mint/vitalik/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "Vitalik Buterin",
+      image:
+        "https://bafybeidibm3ws7t2zmgk4vg7gd3fdfndtu3ywdgk6hpqclgjtqsb266ar4.ipfs.w3s.link/Vitalik%20Card.png",
+      description: "Founder of Ethereum",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/mint/sandeep/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "Sandeep Nailwal",
+      image:
+        "https://bafybeigyect3lemazksr3ko3ok4szkeqkbcm66ftmwvzcnrqxavb6ocdoi.ipfs.w3s.link/Sandeep%20Card.png",
+      description: "Founder of Polygon",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/mint/sandeep/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "Sandeep Nailwal",
+      image:
+        "https://bafybeigyect3lemazksr3ko3ok4szkeqkbcm66ftmwvzcnrqxavb6ocdoi.ipfs.w3s.link/Sandeep%20Card.png",
+      description: "Founder of Polygon",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/mint/anatoly/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "Anatoly Yakovenko",
+      image:
+        "https://bafybeickgawhzcwdmvus6qlilq3i7sfry3vf6n3gjcjw5k4nzsvp74w7am.ipfs.w3s.link/Anatoly%20Card.png",
+      description: "Founder of Solana",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/mint/mert/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "Mert Mumtaj",
+      image:
+        "https://bafybeifsllggg77etqmsiqkta2ms7uhkrazpn3ssslgiy546ci4pnbvrmu.ipfs.w3s.link/Mert%20card.png",
+      description: "Founder of Helius",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/mint/cl207/:address", async (req, res) => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
+      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
+    },
+  };
+
+  const body = JSON.stringify({
+    recipient: "solana:" + req.params.address,
+    metadata: {
+      name: "CL207",
+      image:
+        "https://bafybeigh4qfn72fjardnkyfrqy4b6hsvl7s63lq3ls7rwfreziq53uhy4y.ipfs.w3s.link/CL207%20Card.png",
+      description: "Cat with a hazmat suit",
+    },
+  });
+
+  const response = await axios
+    .post(
+      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
+      body,
+      config
+    )
+    .catch((error) => {
+      return res.status(500).send(err);
+    });
+
+  if (response) {
+    return res.json(response.data);
+  } else {
+    return res.status(500).send("Server error");
+  }
 });
 
 app.listen(8080, () => {
