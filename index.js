29f0470ce0288ce8e1cb9b26dc812785c12e5ce8
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-08-21 03:58:35 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post("/mint/vitalik/:address", async (req, res) => {
       config
     )
     .catch((error) => {
-      return res.status(500).send(err);
+      return res.status(500).send(error);
     });
 
   if (response) {
@@ -109,44 +109,7 @@ app.post("/mint/sandeep/:address", async (req, res) => {
       config
     )
     .catch((error) => {
-      return res.status(500).send(err);
-    });
-
-  if (response) {
-    return res.json(response.data);
-  } else {
-    return res.status(500).send("Server error");
-  }
-});
-
-app.post("/mint/sandeep/:address", async (req, res) => {
-  const config = {
-    headers: {
-      accept: "application/json",
-      "content-type": "application/json",
-      "x-client-secret": "sk_test.2d726b3c.e2b6f527018e88ad299fbfc7c39a1821",
-      "x-project-id": "7f12ef92-e40f-43c9-af10-d4d585656d65",
-    },
-  };
-
-  const body = JSON.stringify({
-    recipient: "solana:" + req.params.address,
-    metadata: {
-      name: "Sandeep Nailwal",
-      image:
-        "https://bafybeigyect3lemazksr3ko3ok4szkeqkbcm66ftmwvzcnrqxavb6ocdoi.ipfs.w3s.link/Sandeep%20Card.png",
-      description: "Founder of Polygon",
-    },
-  });
-
-  const response = await axios
-    .post(
-      "https://staging.crossmint.com/api/2022-06-09/collections/969c472e-c8db-4fb4-8af9-f33c232077c8/nfts",
-      body,
-      config
-    )
-    .catch((error) => {
-      return res.status(500).send(err);
+      return res.status(500).send(error);
     });
 
   if (response) {
@@ -183,7 +146,7 @@ app.post("/mint/anatoly/:address", async (req, res) => {
       config
     )
     .catch((error) => {
-      return res.status(500).send(err);
+      return res.status(500).send(error);
     });
 
   if (response) {
@@ -220,7 +183,7 @@ app.post("/mint/mert/:address", async (req, res) => {
       config
     )
     .catch((error) => {
-      return res.status(500).send(err);
+      return res.status(500).send(error);
     });
 
   if (response) {
@@ -257,7 +220,7 @@ app.post("/mint/cl207/:address", async (req, res) => {
       config
     )
     .catch((error) => {
-      return res.status(500).send(err);
+      return res.status(500).send(error);
     });
 
   if (response) {
